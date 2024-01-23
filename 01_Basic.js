616b99377f68857a2c8bb499a8272ebafe6b35c8
sshhaahh <ameenfalak786@gmail.com>
2024-01-23 13:44:15 +0530

    basics save

---
+++ 01_Basic.js
@@ -0,0 +1,9 @@
+const accountId = 1223;
+let accountEmail = "faleaccoo@gmail";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+// accountId=233;
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.log(typeof accountEmail)
