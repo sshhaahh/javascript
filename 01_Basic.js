c4b6858c683261f76f3ea0c47f66a38174cbd68d
sshhaahh <ameenfalak786@gmail.com>
2024-01-26 00:28:50 +0530

    htsiu

--- 01_Basic.js
+++ 01_Basic.js
@@ -1,13 +1,2 @@
-const accountId = 1223;
-let accountEmail = "faleaccoo@gmail";
-var accountPassword = "12345";
-accountCity = "Jaipur";
-
-// accountId=233;
-
-console.table([accountId,accountEmail,accountPassword,accountCity]);
-console.log(typeof accountEmail)
-const a=Symbol('123')
-console.log(a);
-const v=1111111111111111111n
-
+const num = 1000000;
+console.log(num.toLocaleString('en-IN'))
